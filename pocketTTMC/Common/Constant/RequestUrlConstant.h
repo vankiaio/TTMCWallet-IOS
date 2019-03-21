33390edaa2f4865305e42a98400fab5214357d48
李轶 <liyi@liyideMac-mini.local>
2019-03-21 10:14:33 +0800

    start ttmc modified

--- pocketTTMC/Common/Constant/RequestUrlConstant.h
+++ pocketTTMC/Common/Constant/RequestUrlConstant.h
@@ -14,39 +14,39 @@
 
 
 //#define REQUEST_BASEURL @"https://api6.pocketttmc.top"
-#define REQUEST_BASEURL @"https://221.122.119.226:3033"
+#define REQUEST_BASEURL @"https://119.23.146.214:3033"
 
 
 //#define REQUEST_HTTP_BASEURL @"http://api6.pocketttmc.top"
-//#define REQUEST_HTTP_BASEURL @"http://221.122.119.226:3030"
-#define REQUEST_HTTP_BASEURL @"http://221.122.119.226:3030"
+//#define REQUEST_HTTP_BASEURL @"http://119.23.146.214:3030"
+#define REQUEST_HTTP_BASEURL @"http://119.23.146.214:3030"
 
 //#define REQUEST_HTTP_STATIC_BASEURL @"http://static6.pocketttmc.top"
-#define REQUEST_HTTP_STATIC_BASEURL @"http://221.122.119.226:3030"
+#define REQUEST_HTTP_STATIC_BASEURL @"http://119.23.146.214:3030"
 
 #define REQUEST_REDPACKET_BASEURL @"https://api6.pocketttmc.top/api_oc_redpacket"
 //#define REQUEST_PERSONAL_BASEURL @"https://api6.pocketttmc.top/api_oc_personal/v1.0.0"
-//#define REQUEST_PERSONAL_BASEURL @"http://221.122.119.226:3030/api_oc_personal/v1.0.0"
-#define REQUEST_PERSONAL_BASEURL @"http://221.122.119.226:3030/api_oc_personal/v1.0.0"
+//#define REQUEST_PERSONAL_BASEURL @"http://119.23.146.214:3030/api_oc_personal/v1.0.0"
+#define REQUEST_PERSONAL_BASEURL @"http://119.23.146.214:3030/api_oc_personal/v1.0.0"
 //#define REQUEST_CANDYSYSTEM_BASEURL @"https://api6.pocketttmc.top/api_oc_pe_candy_system"
-#define REQUEST_CANDYSYSTEM_BASEURL @"https://221.122.119.226:3033/api_oc_pe_candy_system"
+#define REQUEST_CANDYSYSTEM_BASEURL @"https://119.23.146.214:3033/api_oc_pe_candy_system"
 //#define REQUEST_CONTRACT_BASEURL @"https://api6.pocketttmc.top"
-#define REQUEST_CONTRACT_BASEURL @"https://221.122.119.226:3033"
+#define REQUEST_CONTRACT_BASEURL @"https://119.23.146.214:3033"
 //#define REQUEST_BP_BASEURL @"https://api6.pocketttmc.top"
-#define REQUEST_BP_BASEURL @"https://221.122.119.226:3033"
+#define REQUEST_BP_BASEURL @"https://119.23.146.214:3033"
 //#define REQUEST_TRANSACTION_RECORDS @"https://history6.pocketttmc.top"
-#define REQUEST_TRANSACTION_RECORDS @"https://221.122.119.226:3033"
+#define REQUEST_TRANSACTION_RECORDS @"https://119.23.146.214:3033"
 //#define REQUEST_HISTORY_HTTP @"http://history6.pocketttmc.top"
-#define REQUEST_HISTORY_HTTP @"http://221.122.119.226:3030"
+#define REQUEST_HISTORY_HTTP @"http://119.23.146.214:3030"
 
 //#define REQUEST_MESSAGE_PUSH_BASEURL @"https://api6.pocketttmc.top"
-#define REQUEST_MESSAGE_PUSH_BASEURL @"https://221.122.119.226:3033"
+#define REQUEST_MESSAGE_PUSH_BASEURL @"https://119.23.146.214:3033"
 
 //#define REQUEST_PAY_CREATEACCOUNT_BASEURL @"http://pay6.pocketttmc.top/oulianttmcaccount"
-#define REQUEST_PAY_CREATEACCOUNT_BASEURL @"http://221.122.119.226:3030/oulianttmcaccount"
+#define REQUEST_PAY_CREATEACCOUNT_BASEURL @"http://119.23.146.214:3030/oulianttmcaccount"
 
 //#define REQUEST_TOKENPAY_BASEURL @"http://pay6.pocketttmc.top/tokenpay"
-#define REQUEST_TOKENPAY_BASEURL @"http://221.122.119.226:3030/tokenpay"
+#define REQUEST_TOKENPAY_BASEURL @"http://119.23.146.214:3030/tokenpay"
 
 
 //// debug 环境
@@ -58,7 +58,6 @@
 //
 //
 ////#define REQUEST_HTTP_BASEURL @"http://api6.pocketttmc.top"
-////#define REQUEST_HTTP_BASEURL @"http://221.122.119.226:3030"
 //#define REQUEST_HTTP_BASEURL @"http://192.178.1.64:3030"
 //
 ////#define REQUEST_HTTP_STATIC_BASEURL @"http://static6.pocketttmc.top"
@@ -66,7 +65,6 @@
 //
 //#define REQUEST_REDPACKET_BASEURL @"https://api6.pocketttmc.top/api_oc_redpacket"
 ////#define REQUEST_PERSONAL_BASEURL @"https://api6.pocketttmc.top/api_oc_personal/v1.0.0"
-////#define REQUEST_PERSONAL_BASEURL @"http://221.122.119.226:3030/api_oc_personal/v1.0.0"
 //#define REQUEST_PERSONAL_BASEURL @"http://192.178.1.64:3030/api_oc_personal/v1.0.0"
 ////#define REQUEST_CANDYSYSTEM_BASEURL @"https://api6.pocketttmc.top/api_oc_pe_candy_system"
 //#define REQUEST_CANDYSYSTEM_BASEURL @"https://192.178.1.64:3033/api_oc_pe_candy_system"
